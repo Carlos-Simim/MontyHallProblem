a8ad0bd56af0a303c1f9a0796b62a0413663ce0e
Carlos-Simim <carlinhossimim@hotmail.com>
2022-07-07 15:13:49 -0300

    Initial commit

---
+++ MontyHallProblem/Porta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontyHallProblem
+{
+    internal class Porta
+    {
+        public bool estado;
+        public bool carro;
+        public bool cabra;
+        public int numero;
+
+        public Porta()
+        {
+            this.estado = false;
+            this.carro = false;
+            this.cabra = true;
+        }
+
+     
+
+    }
+}
---
+++ MontyHallProblem/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontyHallProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int vitorias = 0;
+            int derrotas = 0;
+            
+            Porta p1 = new Porta();
+            Porta p2 = new Porta();
+            Porta p3 = new Porta();
+            Porta escolha;
+            Random r = new Random();
+            int sortearporta;
+
+            for (int i = 0; i < 5; i++)
+            {
+                p1 = new Porta();
+                p1.numero = 1;   
+                p2 = new Porta();
+                p2.numero = 2;
+                p3 = new Porta();
+                p3.numero = 3;
+                escolha = p1;
+                sortearporta = r.Next(1, 3); //Acho que ta com problema aqui
+                switch (sortearporta)
+                {
+                    case 1:
+                        p1.carro = true;
+                        p1.cabra = false;
+                        break;
+
+                    case 2:
+                        p2.carro = true;
+                        p2.cabra = false;
+                        break;
+
+                    case 3:
+                        p3.carro = true;
+                        p3.cabra = false;
+                        break;
+                }
+
+                if (escolha == p1)
+                {
+                    if (p2.carro == true)
+                    {
+                        p3.estado = true;
+                    }
+                    if (p3.carro == true)
+                    {
+                        p2.estado = true;
+                    }
+
+                    if (p1.carro == true)
+                    {
+                        sortearporta = r.Next(2, 3);
+                        switch (sortearporta)
+                        {
+                            case 2:
+                                p2.estado = true;
+                                break;
+
+                            case 3:
+                                p3.estado = true;
+                                break;
+                        }
+                    }
+                }
+
+                if (escolha == p1)
+                {
+                    if(p2.estado == true)
+                    {
+                        escolha = p3;
+                    }
+                    if(p3.estado == true)
+                    {
+                        escolha = p2;
+                    }
+                }
+
+
+                Console.WriteLine("\ncarro de p1: " + p1.carro);
+                Console.WriteLine("carro de p2: " + p2.carro);
+                Console.WriteLine("carro de p3: " + p3.carro);
+
+                Console.WriteLine("Escolha final: " + escolha.numero);
+
+
+                
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
