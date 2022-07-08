3185e8727b62d1320f3a3ff41dba5d5499221521
Carlos-Simim <carlinhossimim@hotmail.com>
2022-07-08 12:10:23 -0300

    -

--- MontyHallProblem/Program.cs
+++ MontyHallProblem/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MontyHallProblem
@@ -14,22 +16,39 @@ namespace MontyHallProblem
             double derrotas = 0;
             double winrate;
             double loserate;
+            Stopwatch stopwatch = new Stopwatch();
             Random r = new Random();
             bool sortear;
-            int quantidade = 10000000;
+            Console.Write("Informe a quantidade desejada de simulações: ");
+            int quantidade;
+            String imput = Console.ReadLine();
 
-            for (int i = 0; i < quantidade; i++)
-            {
-                sortear = Porta.Sortear(r);
-                if (sortear == true)
+            quantidade = Int32.Parse(imput);
+
+            stopwatch.Start();
+
+            Console.Write("\nRealizando simulações... ");
+            
+                for (int i = 0; i < quantidade; i++)
                 {
-                    vitorias = vitorias + 1;
+
+                    sortear = Porta.Sortear(r);
+                    if (sortear == true)
+                    {
+                        vitorias = vitorias + 1;
+                    }
+                    if (sortear == false)
+                    {
+                        derrotas = derrotas + 1;
+                    }
+
+                    
                 }
-                if (sortear == false)
-                {
-                    derrotas = derrotas + 1;
-                }
-            }
+            
+            Console.WriteLine("Finalizado.");
+            Thread.Sleep(2000);
+
+            stopwatch.Stop();
 
             winrate = Porta.getWinrate(vitorias, derrotas, true, quantidade);
             loserate = Porta.getWinrate(vitorias, derrotas, false, quantidade);
@@ -42,6 +61,9 @@ namespace MontyHallProblem
             
             Console.WriteLine("\nLoserate esperado no problema: 33.333%");
             Console.WriteLine("Loserate obtido: " + loserate + "%");
+
+            double tempoDecorrido = stopwatch.ElapsedMilliseconds / 1000;
+            Console.WriteLine("\nTempo de execução: " + tempoDecorrido + "." + stopwatch.ElapsedMilliseconds % 100 + "s");
 
             Console.ReadLine();
 
